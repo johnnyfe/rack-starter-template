f7ca2f67cd2bc192756c394998565f8f9135fe0f
johnny <johnny1194@hotmail.com>
2021-08-08 15:11:23 -0400

    adding soccer update routing

--- app/application.rb
+++ app/application.rb
@@ -28,6 +28,21 @@ class Application
       end
     end
 
+    #Soccer Team Update
+    if req.path.match('/soccer_teams') && req.patch?
+      id = req.path.split('/')[2]
+      body = JSON.parse(req.body.read)
+      begin
+        soccer_team = SoccerTeam.find(id)
+        soccer_team.update(body)
+        return [202, {'Content-Type' => 'application/json'}, [soccer_team.to_json]]
+      rescue ActiveRecord::RecordNotFound
+        return [404, {'Content-Type' => 'application/json'}, [{message: 'soccer team not found'}.to_json]]
+      rescue ActiveRecord::UnknownAttributeError
+        return [422, {'Content-Type' => 'application/json'}, [{message: 'Could not process your update'}.to_json]]
+      end
+    end
+
     res.finish
   end
 
