4dcc04c5f65fc790bd1101add809485ee05ac847
johnny <johnny1194@hotmail.com>
2021-08-09 09:20:36 -0400

    adding assing_team method and creating team and player relationship

--- app/modules/soccer_player.rb
+++ app/modules/soccer_player.rb
@@ -1,3 +1,11 @@
 class SoccerPlayer < ActiveRecord::Base
     belongs_to :soccer_team
+
+    def assign_team(name:)
+        soccer_team = SoccerTeam.find_by(name: name)
+        soccer_team_id =  soccer_team.id
+        soccer_player = SoccerPlayer.find_by(id: self.id)
+        soccer_player.soccer_team_id = soccer_team_id
+        soccer_player.save
+    end
 end
