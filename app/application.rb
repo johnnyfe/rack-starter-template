b6a7edf931aba123cd8476cb8f360e1a92d5897a
johnny <johnny1194@hotmail.com>
2021-08-09 08:41:48 -0400

    adding soccer player delete routing

--- app/application.rb
+++ app/application.rb
@@ -99,6 +99,18 @@ class Application
       end
     end
 
+    #Soccer Player Delete
+
+    if req.path.match('/soccer_players/') && req.delete?
+      id = req.path.split('/')[2]
+      begin
+        soccer_player = SoccerPlayer.find(id)
+        soccer_player.destroy
+        return [202, {'Content-Type' => 'application/json'}, [soccer_player.to_json]]
+      rescue 
+        return [404, {'Content-Type' => 'application/json'}, [{message: 'soccer player not found'}.to_json]]
+      end
+    end
 
     res.finish
   end
